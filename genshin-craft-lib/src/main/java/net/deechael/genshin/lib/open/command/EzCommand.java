bd1420ca631e69242f580b3c54428213e7826234
DeeChael <DeeChael@outlook.com>
2022-10-30 17:46:47 +0800

    fix: forget to add static command creating methods

--- genshin-craft-lib/src/main/java/net/deechael/genshin/lib/open/command/EzCommand.java
+++ genshin-craft-lib/src/main/java/net/deechael/genshin/lib/open/command/EzCommand.java
@@ -1,5 +1,6 @@
 package net.deechael.genshin.lib.open.command;
 
+import net.deechael.genshin.lib.impl.command.EzArgumentImpl;
 import net.deechael.genshin.lib.impl.command.EzCommandImpl;
 import net.deechael.useless.function.functions.Function;
 import org.bukkit.command.CommandSender;
@@ -18,6 +19,34 @@ public interface EzCommand {
 
     static EzCommand literal(String name) {
         return new EzCommandImpl(name);
+    }
+
+    static EzCommand literal(String name, int vanillaPermission) {
+        return new EzCommandImpl(name, vanillaPermission);
+    }
+
+    static EzCommand literal(String name, String bukkitPermission) {
+        return new EzCommandImpl(name, bukkitPermission);
+    }
+
+    static EzCommand literal(String name, int vanillaPermission, String bukkitPermission) {
+        return new EzCommandImpl(name, vanillaPermission, bukkitPermission);
+    }
+
+    static EzCommand argument(EzArgumentType argumentType, String name) {
+        return new EzArgumentImpl(argumentType.type(), name);
+    }
+
+    static EzCommand argument(EzArgumentType argumentType, String name, int vanillaPermission) {
+        return new EzArgumentImpl(argumentType.type(), name, vanillaPermission);
+    }
+
+    static EzCommand argument(EzArgumentType argumentType, String name, String bukkitPermission) {
+        return new EzArgumentImpl(argumentType.type(), name, bukkitPermission);
+    }
+
+    static EzCommand argument(EzArgumentType argumentType, String name, int vanillaPermission, String bukkitPermission) {
+        return new EzArgumentImpl(argumentType.type(), name, vanillaPermission, bukkitPermission);
     }
 
 }
